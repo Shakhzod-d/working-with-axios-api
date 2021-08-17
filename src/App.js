8ba34b9d4f643bed4176fd48ee745ed4cfc43c01
Shakhzod <forworkingappsinmylaptop@gmail.com>
2021-08-17 12:35:18 +0500

    2nd commit, navbar added using react with Link Route, BrowserRouter, Switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import Photos from './components/Photos';
-import SassTutorial from './sassFiles/SassTutorial';
+// import Photos from './components/Photos';
+// import SassTutorial from './sassFiles/SassTutorial';
+import Blog from './Blog';
+import About from './About';
+import Nav from './Nav';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SassTutorial />
-      <Photos />
-    </div>
+    <BrowserRouter>
+      {' '}
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/about" component={About} />
+        </Switch>
+        {/* <About /> */}
+        {/* <SassTutorial />
+        <Photos /> */}
+      </div>
+    </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>This is Home page</p>
+    </div>
+  );
+};
 
 export default App;
